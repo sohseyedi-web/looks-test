b7c1ce81e1296037245e0a0b20a1ba3921c9658d
sohseyedi <sohseyedi@gmail.com>
2025-01-26 14:25:24 +0330

    feat: add Zustand store for managing search state and image data

---
+++ src/store/useStore.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import { ImagesTypes } from "../types/ImagesTypes";
+import { SearchStore } from "../types/StoreTypes";
+
+export const useSearchStore = create<SearchStore>((set, get) => ({
+  search: "",
+  searchHistory: [],
+  loading: false,
+  images: [],
+
+  // actions
+  setSearchTerm: (term) => set({ search: term }),
+  addToSearchHistory: (term: string[]) =>
+    set((state) => ({
+      searchHistory: [...state.searchHistory, ...term],
+    })),
+  setLoading: (loading) => set({ loading }),
+
+  saveSearch: () => {
+    const { searchHistory, search } = get();
+
+    if (search.trim() === "") return;
+    if (!searchHistory.includes(search)) {
+      const updatedHistory = [search, ...searchHistory];
+      set({ searchHistory: updatedHistory });
+      localStorage.setItem("searchHistory", JSON.stringify(updatedHistory));
+    }
+  },
+
+  addToImageData: (term: ImagesTypes[]) => {
+    set((state) => ({
+      images: [...state.images, ...term],
+    }));
+  },
+}));
