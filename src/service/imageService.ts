d33749967ac2bbf2a4689d1f86c427f330c39136
sohseyedi <sohseyedi@gmail.com>
2025-01-26 18:00:20 +0330

    chore: simplify fetchImages

--- src/service/imageService.ts
+++ src/service/imageService.ts
@@ -1,14 +1,10 @@
 import api from "./api";
 
 export default async function fetchImages(query: string) {
-  try {
-    const { data } = await api.get(
-      `/search/photos?page=1&query=${query}&client_id=${
-        import.meta.env.VITE_API_KEY
-      }`
-    );
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+  const { data } = await api.get(
+    `/search/photos?page=1&query=${query}&client_id=${
+      import.meta.env.VITE_API_KEY
+    }`
+  );
+  return data;
 }
