9c026d6f7e3f8151d00f8e435e93434f02621958
sohseyedi <sohseyedi@gmail.com>
2025-01-26 18:51:08 +0330

    chore: LazyLoadImage and add loading state to ImageBox

--- src/components/ImageBox.tsx
+++ src/components/ImageBox.tsx
@@ -1,22 +1,29 @@
-import LazyLoad from "react-lazyload";
+import { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import { ImageItem } from "../types/ImagesTypes";
 
 const ImageBox = ({ image }: ImageItem) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
-    <div className="cursor-pointer">
-      <LazyLoad
-        height={220}
-        offset={200}
-        placeholder={
-          <div className="w-full h-[220px] bg-gray-700 rounded-2xl animate-pulse" />
-        }
-      >
-        <img
-          src={image?.urls?.full}
-          alt={image.alt_description || "Image"}
-          className="object-cover w-full h-[200px] md:h-[260px] sm:h-[220px] rounded-2xl"
-        />
-      </LazyLoad>
+    <div className="cursor-pointer relative">
+      {/* Placeholder */}
+      {!isLoaded && (
+        <div className="w-full h-[220px] bg-zinc-800 rounded-2xl animate-pulse absolute top-0 left-0">
+        </div>
+      )}
+
+      {/* Lazy Loaded Image */}
+      <LazyLoadImage
+        src={image?.urls?.full}
+        alt={image.alt_description || "Image"}
+        effect="opacity"
+        onLoad={() => setIsLoaded(true)}
+        className={`object-cover w-full h-[200px] md:h-[260px] sm:h-[220px] rounded-2xl transition-opacity duration-300 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        }`}
+      />
     </div>
   );
 };
