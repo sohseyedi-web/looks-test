a9b39ac31a5ee663b086a6353aa80df02f185bbc
sohseyedi <sohseyedi@gmail.com>
2025-01-26 14:22:53 +0330

    feat: integrate search state management into TextField component

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,25 +1,24 @@
-import { Dispatch, SetStateAction } from "react";
 import { RiSearch2Line } from "react-icons/ri";
+import { useSearchStore } from "../store/useStore";
 
 type FieldTypes = {
-  value: string;
-  onChange: Dispatch<SetStateAction<string>>;
   onFocus: () => void;
   onBlur: () => void;
 };
 
-const TextField = ({ value, onChange, onFocus, onBlur }: FieldTypes) => {
+const TextField = ({ onFocus, onBlur }: FieldTypes) => {
+  const { search, setSearchTerm } = useSearchStore();
+
   return (
     <div className="w-full flex items-center bg-transparent justify-center gap-3 lg:h-[55px] h-[50px] rounded-[18px] border-2 border-[#3d3d3d] px-3">
       <RiSearch2Line size={26} className="text-zinc-400" />
       <input
-        autoFocus={true}
         type="text"
         placeholder="Search Keywords..."
-        value={value}
+        value={search}
         onFocus={onFocus}
         onBlur={onBlur}
-        onChange={(e) => onChange(e.target.value)}
+        onChange={(e) => setSearchTerm(e.target.value)}
         className="flex-1 bg-transparent outline-none lg:text-lg text-zinc-100 placeholder:text-gray-400"
       />
     </div>
