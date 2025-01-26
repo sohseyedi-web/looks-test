b2c36358271dce77821b8415c05f68c5c7069d76
sohseyedi <sohseyedi@gmail.com>
2025-01-26 14:23:30 +0330

    feat: add SearchHistory component to display and manage search history

---
+++ src/components/SearchHistory.tsx
@@ -0,0 +1,35 @@
+import { useSearchStore } from "../store/useStore";
+
+type SearchHistoryTypes = {
+  show: boolean;
+  setShow : React.Dispatch<React.SetStateAction<boolean>>
+};
+
+const SearchHistory = ({show,setShow} : SearchHistoryTypes) => {
+  const { searchHistory, setSearchTerm } = useSearchStore();
+
+  const handleHistoryClick = (selectedQuery: string) => {
+    setSearchTerm(selectedQuery);
+    setShow(false);
+  };
+
+  return (
+    <div
+      className={`border-2 border-[#3d3d3d] bg-[#101010] mt-3 px-4 py-1 text-zinc-400 rounded-2xl transition-all duration-300 ease-in-out ${
+        show ? "opacity-100 max-h-96" : "opacity-0 max-h-0"
+      } overflow-hidden`}
+    >
+      {searchHistory?.map((item, index) => (
+        <span
+          className="text-2xl block my-2 cursor-pointer hover:bg-[#313131] rounded-lg p-2"
+          key={index}
+          onClick={() => handleHistoryClick(item)}
+        >
+          {item}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default SearchHistory;
