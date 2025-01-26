4e19de07617afc3b43c07ecd5465549d7cd3c521
sohseyedi <sohseyedi@gmail.com>
2025-01-26 14:23:08 +0330

    feat: implement lazy loading for images in ImageBox component

--- src/components/ImageBox.tsx
+++ src/components/ImageBox.tsx
@@ -1,13 +1,22 @@
+import LazyLoad from "react-lazyload";
 import { ImageItem } from "../types/ImagesTypes";
 
 const ImageBox = ({ image }: ImageItem) => {
   return (
-    <div key={image.id} className="cursor-pointer">
-      <img
-        src={image?.urls?.full}
-        alt={image.alt_description}
-        className="object-contain w-full h-[220px] rounded-2xl"
-      />
+    <div className="cursor-pointer">
+      <LazyLoad
+        height={220}
+        offset={200}
+        placeholder={
+          <div className="w-full h-[220px] bg-gray-700 rounded-2xl animate-pulse" />
+        }
+      >
+        <img
+          src={image?.urls?.full}
+          alt={image.alt_description || "Image"}
+          className="object-cover w-full h-[200px] md:h-[260px] sm:h-[220px] rounded-2xl"
+        />
+      </LazyLoad>
     </div>
   );
 };
